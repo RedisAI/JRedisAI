a79a7033f17369032103a226fce071a66cf386c8
filipe oliveira <filipecosta.90@gmail.com>
2020-05-27 11:01:52 +0100

    [add] enforcing https://github.com/google/google-java-format via Git Code Format Maven Plugin (#18)

--- src/main/java/com/redislabs/redisai/RedisAIException.java
+++ src/main/java/com/redislabs/redisai/RedisAIException.java
@@ -1,9 +1,8 @@
 package com.redislabs.redisai;
 
-public class RedisAIException extends RuntimeException{
-  
+public class RedisAIException extends RuntimeException {
+
   public RedisAIException(Exception cause) {
     super(cause);
   }
-
 }
--- src/main/java/com/redislabs/redisai/exceptions/JRedisAIRunTimeException.java
+++ src/main/java/com/redislabs/redisai/exceptions/JRedisAIRunTimeException.java
@@ -3,19 +3,19 @@ package com.redislabs.redisai.exceptions;
 import redis.clients.jedis.exceptions.JedisDataException;
 
 /**
- * An instance of JRedisAIRunTimeException is thrown when RedisAI
- * encounters a runtime error during command execution.
+ * An instance of JRedisAIRunTimeException is thrown when RedisAI encounters a runtime error during
+ * command execution.
  */
 public class JRedisAIRunTimeException extends JedisDataException {
-    public JRedisAIRunTimeException(String message) {
-        super(message);
-    }
+  public JRedisAIRunTimeException(String message) {
+    super(message);
+  }
 
-    public JRedisAIRunTimeException(Throwable cause) {
-        super(cause);
-    }
+  public JRedisAIRunTimeException(Throwable cause) {
+    super(cause);
+  }
 
-    public JRedisAIRunTimeException(String message, Throwable cause) {
-        super(message, cause);
-    }
+  public JRedisAIRunTimeException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
