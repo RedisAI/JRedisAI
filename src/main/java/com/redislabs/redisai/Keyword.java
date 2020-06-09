1e4c613c3fcb63b4f22910bbc09205658dc391cc
filipecosta90 <filipecosta.90@gmail.com>
2020-06-09 11:17:11 +0100

    [fix] fixes per PR review

--- src/main/java/com/redislabs/redisai/Keyword.java
+++ src/main/java/com/redislabs/redisai/Keyword.java
@@ -4,26 +4,30 @@ import redis.clients.jedis.commands.ProtocolCommand;
 import redis.clients.jedis.util.SafeEncoder;
 
 public enum Keyword implements ProtocolCommand {
-  INPUTS("INPUTS"),
-  OUTPUTS("OUTPUTS"),
-  META("META"),
-  VALUES("VALUES"),
-  BLOB("BLOB"),
-  SOURCE("SOURCE"),
-  RESETSTAT("RESETSTAT"),
-  TAG("TAG"),
-  BATCHSIZE("BATCHSIZE"),
-  MINBATCHSIZE("MINBATCHSIZE"),
-  BACKENDSPATH("BACKENDSPATH"),
-  LOADBACKEND("LOADBACKEND"),
-  LOAD("LOAD"),
-  PERSIST("PERSIST"),
+  INPUTS,
+  OUTPUTS,
+  META,
+  VALUES,
+  BLOB,
+  SOURCE,
+  RESETSTAT,
+  TAG,
+  BATCHSIZE,
+  MINBATCHSIZE,
+  BACKENDSPATH,
+  LOADBACKEND,
+  LOAD,
+  PERSIST,
   PIPE("|>");
 
   private final byte[] raw;
 
-  Keyword(String s) {
-    raw = SafeEncoder.encode(s);
+  Keyword() {
+    raw = SafeEncoder.encode(this.name());
+  }
+
+  Keyword(String encodeStr) {
+    raw = SafeEncoder.encode(encodeStr);
   }
 
   public byte[] getRaw() {
